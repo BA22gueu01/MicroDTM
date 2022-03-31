5839fe2d66ad67b175e5f9b27d959b0bb006074b
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-03-31 16:28:56 +0200

    Made Logging and Service

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-import requests  
+import requests
+import logging
+import time
 
 PROMETHEUS = 'http://10.161.2.161:31090/'
 
-response =requests.get(PROMETHEUS + '/api/v1/query', params={'query': 'counter_status_200_carts_customerId_items'})
+print("Start Loop")
 
-print(response)
+while True:
+    response =requests.get(PROMETHEUS + '/api/v1/query', params={'query': 'counter_status_200_carts_customerId_items'})
+    responseJson = response.json()
+    data = responseJson["data"]
+    result = data["result"][0]
+    values = result["value"]
+    print("At time", values[0], "there were", values[1], "successful requests!")
+    time.sleep(30)
