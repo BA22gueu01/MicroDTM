8031b2626da05f47f7f7fe599c7657fa8e368c3d
valeriadr <derigval@students.zhaw.ch>
2022-04-18 01:38:25 +0200

    fixed redundant sentence

--- src/ApparmorCheck.py
+++ src/ApparmorCheck.py
@@ -5,8 +5,6 @@ class ApparmorCheck:
 
     def checkApparmor(self):
 
-        print('Check log level for: warning, error and fatal messages')
-
         # output = subprocess.check_output(["cat", "/sys/module/apparmor/parameters/enabled"])
         output = "Y"
         if output == "Y":
