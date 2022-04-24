a9e1c131181f498a94931a77408e6f0b4be12486
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-25 00:52:58 +0200

    Fixed small error

--- src/ApparmorCheck.py
+++ src/ApparmorCheck.py
@@ -6,7 +6,7 @@ class ApparmorCheck:
     def checkApparmor(self):
 
         #output = subprocess.check_output(["cat", "/sys/module/apparmor/parameters/enabled"])
-        print(subprocess.check_output(["kubectl", "exec", "-n", "sock-shop queue-master-6bf76bbfc-4hcwf", "--", "cat", "/sys/module/apparmor/parameters/enabled"]))
+        print(subprocess.check_output(["kubectl", "exec", "-n", "sock-shop", "queue-master-6bf76bbfc-4hcwf", "--", "cat", "/sys/module/apparmor/parameters/enabled"]))
         output = "Y"
         if output == "Y":
             return 5
