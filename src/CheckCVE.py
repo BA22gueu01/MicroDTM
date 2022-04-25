1da738e4b4a3bdc2294a7533fa39ee365f42897a
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-25 21:44:57 +0200

    Trivy Sample Request

--- src/CheckCVE.py
+++ src/CheckCVE.py
@@ -8,7 +8,6 @@ class CheckCVE:
 
         imgs = subprocess.check_output(["kubectl", "get", "pods", "-n", "sock-shop"])
         print(imgs)
-        for img in imgs:
-            result = subprocess.check_output(["trivy", "-q", "image", "--light", "--no-progress", "--severity", "CRITICAL", img])
-            print(result)
+        result = subprocess.check_output(["trivy", "-q", "image", "--light", "--no-progress", "--severity", "CRITICAL", "sock-shop\queue-master-6bf76bbfc-4hcwf"])
+        print(result)
         return 0
