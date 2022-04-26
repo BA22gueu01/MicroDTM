fe3a43e4a191d0bafae60c770f045caf34aa250c
valeriadr <derigval@students.zhaw.ch>
2022-04-26 19:50:35 +0200

    added container parameter to LogLevelCheck function

--- src/LogLevelCheck.py
+++ src/LogLevelCheck.py
@@ -4,9 +4,8 @@ class LogLevelCheck:
 
     def checkLoglevel(self):
 
-        #output = subprocess.check_output(["microk8s.kubectl", "logs", "carts-77b9db4898-27w9m", "--namespace=sock-shop", "--v=1", "|", "grep", "-iE", "'(warning|error|fatal)'", "|", "wc", "-l"])
         try:
-            output = subprocess.check_output(["kubectl", "logs", "queue-master-6bf76bbfc-4hcwf", "--namespace=sock-shop", "--v=1", "|", "grep", "-E", "'(warning|error|fatal)'", "|", "wc", "-l"])
+            output = subprocess.check_output(["kubectl", "logs", "queue-master-6bf76bbfc-4hcwf", "--container=queue-master", "--namespace=sock-shop", "--v=1", "|", "grep", "-i", "-E", "'(warning|error|fatal)'", "|", "wc", "-l"])
             print(output)
         except Exception as e:
             print(e)
