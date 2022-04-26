8632ab725c050684a7f78455330e5b4ec29a9fb9
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-26 09:55:09 +0200

    Changed AppArmor Check

--- src/ApparmorCheck.py
+++ src/ApparmorCheck.py
@@ -6,9 +6,10 @@ class ApparmorCheck:
     def checkApparmor(self):
 
         output = subprocess.check_output(["kubectl", "exec", "-n", "sock-shop", "queue-master-6bf76bbfc-4hcwf", "--", "cat", "/sys/module/apparmor/parameters/enabled"])
+        output = output.decode()
         print(output)
         #output = "Y"
-        if "Y" in output:
+        if 'Y' in output:
             return 5
         else:
             return -5
