ffb2baae1c7d32ed055bb1068c1c425f874f0180
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-26 09:06:24 +0200

    Try Catch arround found error

--- src/LogLevelCheck.py
+++ src/LogLevelCheck.py
@@ -6,9 +6,13 @@ class LogLevelCheck:
     def checkLoglevel(self):
 
         #output = subprocess.check_output(["microk8s.kubectl", "logs", "carts-77b9db4898-27w9m", "--namespace=sock-shop", "--v=1", "|", "grep", "-iE", "'(warning|error|fatal)'", "|", "wc", "-l"])
-        output = subprocess.check_output(["kubectl", "logs", "queue-master-6bf76bbfc-4hcwf", "--namespace=sock-shop", "--v=1", "|", "grep", "-iE", "'(warning|error|fatal)'", "|", "wc", "-l"])
-        print(output)
-        #output = 20
+        try:
+            output = subprocess.check_output(["kubectl", "logs", "queue-master-6bf76bbfc-4hcwf", "--namespace=sock-shop", "--v=1", "|", "grep", "-E", "'(warning|error|fatal)'", "|", "wc", "-l"])
+            print(output)
+        except Error as e:
+            print(e)
+            output = 20
+
         # to be updated
         if output > 25:
             return -5
