a276c77df7cfe05b0576db7011ea7feb93a56465
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-28 16:21:52 +0200

    Fixed getContainers

--- src/GetPods.py
+++ src/GetPods.py
@@ -9,20 +9,17 @@ class GetPods:
         output = subprocess.Popen(["kubectl", "get", "pods", "-n", "sock-shop", "--no-headers",
                                             "--field-selector=status.phase=Running", "-o",
                                             "custom-columns=:metadata.name"], stdout=subprocess.PIPE)
-        # output.wait()
-        print("POPEN")
         for line in output.stdout.readlines():
             line = line.decode().strip('\n')
-            print(line)
             pods.append(line)
 
         return pods
 
-    def getContainers(podName):
+    def getContainers(self, podName):
         containers = []
         output = subprocess.Popen(["kubectl", "describe", "pod/"+podName, "-n", "sock-shop"], stdout=subprocess.PIPE)
-        output.wait()
         for line in output.stdout.readlines():
+            line = line.decode().strip('\n')
             print(line)
             containers.append(line)
 
