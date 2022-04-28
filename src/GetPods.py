83c78074ec275f3cdbdcf612eea77fab0069f2f2
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-04-28 14:18:10 +0200

    Added communicate

--- src/GetPods.py
+++ src/GetPods.py
@@ -4,9 +4,9 @@ class GetPods:
 
     def getPods(self):
         pods = []
-        output = subprocess.Popen(["kubectl", "get", "pods", "-o", "custom-columns=\":metadata.name\"", "--no-headers",
+        command = subprocess.Popen(["kubectl", "get", "pods", "-o", "custom-columns=\":metadata.name\"", "--no-headers",
                                    "--field-selector=status.phase=Running", "-n", "sock-shop"], stdout=subprocess.PIPE)
-        output.wait()
+        output = command.communicate()
         print(output)
         print(output.stdout)
         for line in output.stdout.readlines():
