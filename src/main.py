a088520b0db87c6f747ae45bf5964b875c32d2b0
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 00:28:33 +0200

    Changed Schedule Time back

--- src/main.py
+++ src/main.py
@@ -71,8 +71,8 @@ def main():
     print("Main Call!")
     initialCalculation()
     schedule.every(30).seconds.do(update)
-    schedule.every(1).minutes.do(hourlyUpdate)
-    schedule.every(2).minutes.do(dailyUpdate)
+    schedule.every().hour.do(hourlyUpdate)
+    schedule.every().day.do(dailyUpdate)
 
     while True:
         schedule.run_pending()
