ecf3f137f9dcc36c176a40768bf06b41abe17663
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 19:05:06 +0200

    Testing DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -5,7 +5,7 @@ class DBRequest:
 
     def makeRequest(self, tableName):
         request = subprocess.Popen(["kubectl", "exec", "-n", "sock-shop", "catalogue-db-86c68f4757-4tvzt",
-                                   "--container", "catalogue-db", "--", "/bin/bash", "-c",
+                                   "--container", "catalogue-db", "--", "bash", "-c",
                                    "\"mysql -u catalogue_user -pdefault_password socksdb -e \'select * from tag;\'\""], stdout=subprocess.PIPE,
                                    stdin=subprocess.PIPE)
         print("Opened")
