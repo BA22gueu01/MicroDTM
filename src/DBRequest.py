4cb1625c790b24b5f4f82694d900ad9a9bfebf5d
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 18:14:35 +0200

    Testing DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -5,5 +5,8 @@ import pexpect
 class DBRequest:
 
     def makeRequest(self, tableName):
-        request = pexpect.spawn('kubectl exec -i -t -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db -- /bin/bash -c "mysql -u catalogue_user -pdefault_password socksdb"')
+        request = pexpect.spawn('kubectl exec -i -t -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db '
+                                '-- /bin/bash -c "mysql -u catalogue_user -pdefault_password socksdb"')
+        request.sendline('select * from tag;')
         print(request.read())
+
