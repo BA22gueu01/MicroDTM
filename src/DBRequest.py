e79ff1b1c69f8f21229682c0bfa1f323375b50a3
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 20:12:43 +0200

    Improving DB Request

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -4,7 +4,7 @@ import subprocess
 class DBRequest:
 
     def makeRequest(self, tableName):
-        mysqlCommand = "mysql -u catalogue_user -pdefault_password socksdb -e 'select * from" + tableName + ";'"
+        mysqlCommand = "mysql -u catalogue_user -pdefault_password socksdb -e 'select * from " + tableName + ";'"
         request = subprocess.Popen(["kubectl", "exec", "-n", "sock-shop", "catalogue-db-86c68f4757-4tvzt",
                                    "--container", "catalogue-db", "--", "bash", "-c", mysqlCommand],
                                    stdout=subprocess.PIPE)
