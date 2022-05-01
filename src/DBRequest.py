9244825e65fb24de3585760a1a5ee6e8ffabb361
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 18:09:24 +0200

    Testing DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -5,11 +5,5 @@ import pexpect
 class DBRequest:
 
     def makeRequest(self, tableName):
-        request = pexpect.spawn('kubectl exec -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db -- /bin/bash -c mysql -u catalogue_user -p')
+        request = pexpect.spawn('kubectl exec -i -t -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db -- /bin/bash -c "mysql -u catalogue_user -pdefault_password socksdb"')
         print(request.read())
-        i = request.expect([pexpect.TIMEOUT, "Enter password:"])
-
-
-
-
-
