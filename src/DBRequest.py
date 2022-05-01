5a08272c18f8fb8482c0bb010cd0c647c1dac096
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 18:26:31 +0200

    Testing DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -5,8 +5,10 @@ import pexpect
 class DBRequest:
 
     def makeRequest(self, tableName):
-        request = subprocess.Popen('kubectl exec -i -t -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db '
-                                '-- /bin/bash -c "mysql -u catalogue_user -pdefault_password socksdb"', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        request = subprocess.Popen(["kubectl", "exec", "-i", "-t", "-n", "sock-shop catalogue-db-86c68f4757-4tvzt",
+                                   "--container", "catalogue-db", "--", "/bin/bash", "-c",
+                                   "\"mysql -u catalogue_user -pdefault_password socksdb\""], stdout=subprocess.PIPE,
+                                   stdin=subprocess.PIPE)
         print("Opened")
         request.communicate("select * from tag;")
         print(request.stdout.readlines())
