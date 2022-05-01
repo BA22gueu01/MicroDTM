1e1d938fbad3f5644a24afc1d5f774179ad8c8e1
valeriadr <derigval@students.zhaw.ch>
2022-05-01 22:15:06 +0200

    CorrectnessCatalogueCalculation

--- src/CorrectnessGradeCalculation.py
+++ src/CorrectnessGradeCalculation.py
@@ -1,5 +1,6 @@
 import APIRequest
 import DBRequest
+import GetPods
 
 class CorrectnessGradeCalculation:
 
@@ -8,15 +9,31 @@ class CorrectnessGradeCalculation:
         self.callCorrectnessWeight = 1
         self.apiRequest = APIRequest.APIRequest(sockshop)
         self.dbRequest = DBRequest.DBRequest()
+        self.getPods = GetPods.GetPods()
 
     def calculateGrade(self):
         return self.callCorrectnessWeight * self.callCorrectnessGrade
 
     def calculateCallCorrectnessGrade(self):
         print(self.apiRequest.makeRequest("tags"))
-        print(self.dbRequest.makeRequest("tag"))
+        self.calculateCatalogueCorrectness()
         self.callCorrectnessGrade = 0
         print("callCorrectnessGrade: ", self.callCorrectnessGrade)
+
+    def calculateCatalogueCorrectness(self):
+        pods = self.getPods.getPods()
+        podName = ""
+        containerName = ""
+
+        for pod in pods:
+            if "catalogue" in pod and "db" in pod:
+                podName = pod
+                print(podName)
+
+        containers = self.getPods.getContainers()
+
+        for container in containers:
+            print(container)
 
     def hourlyUpdate(self):
         self.calculateCallCorrectnessGrade()
--- src/DBRequest.py
+++ src/DBRequest.py
@@ -3,14 +3,15 @@ import subprocess
 
 class DBRequest:
 
-    def makeRequest(self, tableName):
+    def makeRequest(self, podName, containerName, tableName):
         mysqlCommand = "mysql -u catalogue_user -pdefault_password socksdb -e 'select * from " + tableName + ";'"
-        request = subprocess.Popen(["kubectl", "exec", "-n", "sock-shop", "catalogue-db-86c68f4757-4tvzt",
-                                   "--container", "catalogue-db", "--", "bash", "-c", mysqlCommand],
+        request = subprocess.Popen(["kubectl", "exec", "-n", "sock-shop", podName,
+                                   "--container", containerName, "--", "bash", "-c", mysqlCommand],
                                    stdout=subprocess.PIPE)
         answer = []
         for line in request.stdout.readlines():
             line = line.decode().strip('\n')
+            print(line)
             answer.append(line)
 
         return answer
