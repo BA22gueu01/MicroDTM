0cb061da2ab4143764e917a1e891678ded239314
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 17:16:10 +0200

    Improved DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -7,7 +7,7 @@ class DBRequest:
     def makeRequest(self, tableName):
         request = pexpect.spawn('kubectl exec -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db -- /bin/bash')
         print(request.read())
-        request.sendline('mysql -u catalogue_user -p')
+        request.sendline('mysql -u catalogue_user -p \n')
         print(request.read())
         request.expect("Enter password:")
         request.sendline('default_password')
