8848255371b6e43699fcc583fe88fa036fd669df
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-01 17:44:56 +0200

    Improved DB Command

--- src/DBRequest.py
+++ src/DBRequest.py
@@ -7,19 +7,27 @@ class DBRequest:
     def makeRequest(self, tableName):
         request = pexpect.spawn('kubectl exec -n sock-shop catalogue-db-86c68f4757-4tvzt --container catalogue-db -- /bin/bash')
         print(request.read())
-        request.sendline('mysql -u catalogue_user -p \n')
+        request.sendline('mysql -u catalogue_user -p')
         print(request.read())
         request.sendeof()
         print(request.read())
-        request.expect("Enter password:")
-        request.sendline('default_password')
-        print(request.read())
-        request.sendline('USE socksdb;')
-        print(request.read())
-        request.sendline('select * from ' + tableName + ';')
-        print(request.read())
-        request.sendline('exit')
-        request.sendline('exit')
+        i = request.expect([pexpect.TIMEOUT, "Enter password:"])
+        if i == 0:
+            print("Got unexpected output: %s %s" % (request.before, request.after))
+            request.send(chr(13))
+            print(request.read())
+            i = request.expect([pexpect.TIMEOUT, "Enter password:"])
+            print(i)
+
+        else:
+            request.sendline('default_password')
+            print(request.read())
+            request.sendline('USE socksdb;')
+            print(request.read())
+            request.sendline('select * from ' + tableName + ';')
+            print(request.read())
+            request.sendline('exit')
+            request.sendline('exit')
 
 
 
