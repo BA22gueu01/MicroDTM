3c2e93107deb30337d2636ef6d523b569577a47a
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-02 23:40:43 +0200

    Analyzing new Reliability Calls

--- src/AvailabilityGradeCalculation.py
+++ src/AvailabilityGradeCalculation.py
@@ -58,7 +58,7 @@ class AvailabilityGradeCalculation:
             counter = 0
             i = length - x
             for value in uptimeValues:
-                grade = grade + self.calculateUptimeGrade(value[x], value[x + 1])
+                grade = grade + self.calculateUptimeGrade(value[x + 1], value[x])
                 counter = counter + 1
             grade = grade / counter
             self.addNewGrade(grade)
--- src/PrometheusRequest.py
+++ src/PrometheusRequest.py
@@ -50,13 +50,16 @@ class PrometheusRequest:
         elif "_history" in requestParam:
             print("history")
             requestParam = requestParam.strip("_history") + '{kubernetes_namespace="sock-shop"}[1d:1h]'
+            print(requestParam)
             prometheusResponse = requests.get(self.PROMETHEUS + '/api/v1/query', params={'query': requestParam})
         else:
             requestParam = requestParam + '{kubernetes_namespace="sock-shop"}[2h:1h]'
             prometheusResponse = requests.get(self.PROMETHEUS + '/api/v1/query', params={'query': requestParam})
 
         prometheusResponseJson = prometheusResponse.json()
+        print(prometheusResponseJson)
         data = prometheusResponseJson["data"]
+        print(data)
 
         # Check if Prometheus result is empty
         result = []
