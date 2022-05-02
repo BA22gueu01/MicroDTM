c47024a4e8f80fa7e317cf7f0e97b1e0a646b951
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-02 22:01:38 +0200

    Added History to AvailabilityGrade

--- src/AvailabilityGradeCalculation.py
+++ src/AvailabilityGradeCalculation.py
@@ -1,4 +1,5 @@
 import PrometheusRequest
+import numpy
 
 
 class AvailabilityGradeCalculation:
@@ -7,15 +8,21 @@ class AvailabilityGradeCalculation:
         self.prometheusRequest = PrometheusRequest.PrometheusRequest(prometheus)
         self.uptimeGrade = 0
         self.uptimeWeight = 1
+        self.uptimeGrades = numpy.zeros(24)
 
     def calculateGrade(self):
+        print(self.uptimeGrades)
+        print(numpy.average(self.uptimeGrades))
+        return self.uptimeWeight * numpy.average(self.uptimeGrades)
 
-        return self.uptimeWeight * self.uptimeGrade
+    def calculateUptime(self, firstValue, secondValue):
 
-    def calculateUptimeGrade(self):
+        return (firstValue[0] - secondValue[0]) / ((firstValue[1] - secondValue[1]) * 1000)
+
+
+
         counter = 1
         uptimeGrade = []
-        uptimeValues = self.prometheusRequest.makeRequest("uptime")
 
         # time = seconds, uptime values = milliseconds
         # denominator to milliseconds so that actualTime and uptimeGrade have the same unit of time
@@ -28,8 +35,38 @@ class AvailabilityGradeCalculation:
         self.uptimeGrade = sum(uptimeGrade) / len(uptimeGrade)
         print("uptime Grade: ", self.uptimeGrade)
 
+    def addNewGrade(self, newGrade):
+        length = len(self.uptimeGrades) - 1
+        for x in range(length):
+            self.uptimeGrades[x] = self.uptimeGrades[x + 1]
+        self.uptimeGrades[length] = newGrade
+
     def update(self):
-        self.calculateUptimeGrade()
+        uptimeValues = self.prometheusRequest.makeRequest("uptime")
+        grade = 0
+        counter = 0
+        print(uptimeValues)
+        for value in uptimeValues:
+            print(value)
+            grade = grade + self.calculateUptime(value[0], value[1])
+            counter = counter + 1
+        grade = grade/counter
+        print("uptime Grade: ", grade)
+        self.addNewGrade(grade)
 
     def initialCalculation(self):
+        uptimeValues = self.prometheusRequest.makeRequest("uptime_history")
+        length = len(uptimeValues[0]) - 1
+        for x in range(length):
+            grade = 0
+            counter = 0
+            i = length - x
+            for value in uptimeValues:
+                grade = grade + self.calculateUptime(value[x], value[x + 1])
+                counter = counter + 1
+            grade = grade / counter
+            print("uptime Grade: ", grade)
+            self.addNewGrade(grade)
+
+
         self.calculateUptimeGrade()
