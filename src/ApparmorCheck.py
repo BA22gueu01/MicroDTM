fc4b318a9c9b8f8d13fa635db442e4399647f28c
valeriadr <derigval@students.zhaw.ch>
2022-05-06 20:11:37 +0200

    suppress subprocess output in log file, apparmor

--- src/ApparmorCheck.py
+++ src/ApparmorCheck.py
@@ -28,7 +28,8 @@ class ApparmorCheck:
 
         try:
             output = subprocess.check_output(["kubectl", "exec", "-n", "sock-shop", podName, "--container",
-                                              containerName, "--", "cat", "/sys/module/apparmor/parameters/enabled"])
+                                              containerName, "--", "cat", "/sys/module/apparmor/parameters/enabled"],
+                                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             output = output.decode()
 
         except Exception as e:
