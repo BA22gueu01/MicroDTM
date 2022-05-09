8cdd50746f95ed55a3e6f29ba99b94265fe5d02d
valeriadr <derigval@students.zhaw.ch>
2022-05-09 13:09:36 +0200

    new website

--- src/SecurityGradeCalculation.py
+++ src/SecurityGradeCalculation.py
@@ -19,7 +19,7 @@ class SecurityGradeCalculation:
         print("ApparmorGrade: ", self.apparmorGrade)
 
     def calculateCertificateGrade(self):
-        certificateCheck = CertificateCheck.CertificateCheck("office.ypsomed.com", "443")
+        certificateCheck = CertificateCheck.CertificateCheck("survey.ypsomed.com", "443")
         self.certificateGrade = certificateCheck.checkCertificate()
         print("CertificateGrade: ", self.certificateGrade)
 
