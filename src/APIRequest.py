a05ed64e1a9ab7b9020500a19f13a07b41f44659
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-05-28 00:14:43 +0200

    Catch API Exception

--- src/APIRequest.py
+++ src/APIRequest.py
@@ -7,17 +7,21 @@ class APIRequest:
         self.sockshop = sockshop
 
     def makeRequest(self, requestParam):
-        response = requests.get(self.sockshop + requestParam)
-        responseJson = response.json()
-        if requestParam == "tags":
-            answer = responseJson["tags"]
-        elif requestParam == "catalogue":
-            answer = []
-            for element in responseJson:
-                jsonLine = "id:" + element["id"] + ",name:" + element["name"] + ",description:" + element["description"] \
+        try:
+            response = requests.get(self.sockshop + requestParam)
+            responseJson = response.json()
+            if requestParam == "tags":
+                answer = responseJson["tags"]
+            elif requestParam == "catalogue":
+                answer = []
+                for element in responseJson:
+                    jsonLine = "id:" + element["id"] + ",name:" + element["name"] + ",description:" + element["description"] \
                            + ",price:" + str(element["price"]) + ",count:" + str(element["count"]) + ","
-                answer.append(jsonLine)
-        else:
-            answer = responseJson
+                    answer.append(jsonLine)
+            else:
+                answer = responseJson
+        except Exception as e:
+            print(e)
+            answer = [0]
 
         return answer
