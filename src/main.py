c2347ef014fcea3f6a33f0cf42aaac6c3a1e7c3a
vogtrap1 <vogtrap1@students.zhaw.ch>
2022-06-30 21:19:04 +0200

    Changed small Demo error

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ PROMETHEUS = 'http://10.161.2.161:31090/'
 SOCKSHOP = 'http://10.161.2.161:30001/'
 UPDATE_INTERVAL = 5
 HISTORIC_DATA = 24
-CERTIFICATE_URL = "ba.ypsomed.com"
+CERTIFICATE_URL = "https://ba.ypsomed.com"
 EXTERN_URL = ["zhaw.ch"]
 
 trustScore = []
